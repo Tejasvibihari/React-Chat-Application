8bf8b0ed3e2cda0d5ce8086d0c17fd7f2dd210e3
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-03-21 18:58:35 +0530

    Nav bar created and chating ui made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+
 
 export default function App() {
   return (
     <>
-
+      <Navbar />
+      <Sidebar />
     </>
   )
 }
---
+++ client/src/Components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import Divider from '@mui/material/Divider';
+import Avatar from '@mui/material/Avatar';
+
+export default function Sidebar() {
+    return (
+        <div className="flex h-screen">
+            <div className="w-1/4 bg-gray-200">
+                <div>
+                    <h2 className="p-4">Contacts</h2>
+                    <Divider />
+                    <ul>
+                        <li className="cursor-pointer p-4 hover:bg-gray-300">
+                            <div className='flex flex-row justify-start items-center'>
+                                <div>
+                                    <Avatar alt="Tejasvi Bihari" src="/static/images/avatar/1.jpg" />
+                                </div>
+                                <div className='ml-6 hidden md:flex'>
+                                    Tejasvi Bihari
+                                </div>
+                            </div>
+                            <div className='mt-3'>
+                                <Divider />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="w-3/4 bg-gray-100">
+                <div>
+                    <h2 className="p-4">Chat with</h2>
+                    {/* Chat messages go here */}
+                </div>
+            </div>
+        </div >
+    );
+}
